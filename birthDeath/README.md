aba2deb59527d9b788f1c3e3d7c136efede5df63
ghart-IDM <ghart@idmod.org>
2023-08-15 08:35:28 -0800

    Adding the first birth-death script

---
+++ birthDeath/README.md
@@ -0,0 +1,4 @@
+# Birth Death Trees
+Scripts for characterization and evaluation of summary statistics for phylogenetic trees.
+
+The main script is run_experiments.py. This script generates a database containing the result of multiple summary statistics for different simulated phhylogenetic trees. Once the database is generated, the build_report.py script generates a pdf file with a group of figures that can be helpful for analysis.
---
+++ birthDeath/run_experiments.py
@@ -0,0 +1,178 @@
+'''
+Grid sweep for computing summary statistics of simulated phylogenetic trees. The
+trees are generated using a birth-death process.
+'''
+import os
+import time
+import numpy 
+import pandas
+
+import ngesh
+import optuna
+
+import phylomodels.features.trees
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+
+
+#-------------------------------------------------------------------------------
+# Parameters
+#-------------------------------------------------------------------------------
+
+# Tree generation
+birth_rate_bounds = [ 0.25, 10 ]
+death_rate_bounds = [ 0.25, 10 ]
+step_size = 0.25
+n_leaves = 50   
+
+# Summary statistics
+path_to_summary_statistics = '/home/rnunez/work/phylomodels/envs/phylomodels-20210921-trees/phyloModels/phylomodels/features/trees'
+
+
+# Optimization
+n_trials = 2500  # Number of runs
+n_jobs   = 8  # Number of cores for parallel runs; use -1 for all cores
+name     = "20210929-tree-summary-stats-rev0"
+storage  = f'sqlite:///{name}.db'
+#-------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------
+# Experiment definition
+#-------------------------------------------------------------------------------
+def run_experiment( params=None ):
+    ''' Definition of a single experiment'''
+
+    # Set parameters
+    if params == None:
+        params = {}
+    min_leaves = params.get( 'n_leaves'  , n_leaves )
+    birth      = params.get( 'birth_rate', 1        )
+    death      = params.get( 'death_rate', 0.5      )
+    seed       = params.get( 'rand_seed' , 0        )
+
+    # Generate tree
+    print( "... generating tree with birth = ", birth, " and death = ", death )
+    tic = time.time()
+    try:
+        tree = ngesh.random_tree.gen_tree( min_leaves = min_leaves,
+                                           birth      = birth,
+                                           death      = death,
+                                           seed       = seed
+                                          )
+    except RuntimeError as e:
+        print( "... ERROR: generating tree with birth = ", birth, 
+               " and death = ", death, " --- ", e )
+        return -1, None, None
+    toc = time.time() - tic
+    
+    # Compute summary statistics
+    print( "... computing summary statistics for tree with birth = ", birth, 
+           " and death = ", death )
+    tree_summary_stats = pandas.DataFrame() 
+    i=0
+    for filename in os.listdir( path_to_summary_statistics ):
+        if filename.startswith("__") or (not filename.endswith(".py")):
+            continue
+        
+        # Get pointer to function
+        function_name = filename[:-3]
+        this_module = getattr( phylomodels.features.trees, function_name)
+        this_function = getattr( this_module, function_name )
+        
+        # Call function
+        #print( '    computing ', function_name )
+        out = this_function(tree)
+        
+        # Update consolidated dataframe
+        tree_summary_stats = pandas.concat( [tree_summary_stats, out], axis=1 )
+       
+        '''
+        i+=1
+        if i>3:
+           break
+        '''
+        
+    # Finalize and return    
+    status = 0
+    return status, tree_summary_stats, toc
+
+
+
+
+def objective(trial):
+    ''' Define the objective for Optuna '''
+    params = {}
+
+    params['birth_rate'] = trial.suggest_float( 'birth_rate', 
+                                                birth_rate_bounds[0], 
+                                                birth_rate_bounds[1],
+                                                step = step_size
+                                               )
+    params['death_rate'] = trial.suggest_float( 'death_rate', 
+                                                death_rate_bounds[0], 
+                                                death_rate_bounds[1],
+                                                step = step_size
+                                               )
+    params['rand_seed' ] = trial.number
+    
+    status, out, toc_tree = run_experiment( params )
+    
+    if status < 0:
+        cost = numpy.inf
+    else:
+        cost = 0
+        trial.set_user_attr( 'time_tree_generation', toc_tree )
+        for key, value in out.iloc[0].to_dict().items():
+            trial.set_user_attr( key, value )
+    
+
+    
+    return cost
+
+
+
+
+def make_study():
+    ''' Make a study '''
+
+    
+    birth_num = 1+int( (birth_rate_bounds[1] - birth_rate_bounds[0])/step_size )
+    death_num = 1+int( (death_rate_bounds[1] - death_rate_bounds[0])/step_size )
+    
+    search_space = { 'birth_rate': numpy.linspace( birth_rate_bounds[0], 
+                                                   birth_rate_bounds[1],
+                                                   birth_num
+                                                  ),
+                     'death_rate': numpy.linspace( death_rate_bounds[0], 
+                                                   death_rate_bounds[1],
+                                                   death_num
+                                                  )
+                    }
+    
+    sampler = optuna.samplers.GridSampler( search_space )
+    output = optuna.create_study( sampler        = sampler,
+                                  storage        = storage, 
+                                  study_name     = name, 
+                                  load_if_exists = True
+                                 )
+    
+    return output
+
+
+
+
+if __name__ == '__main__':
+    #run_sim()
+    #compare_scenarios()
+    
+    make_study()
+    study = optuna.load_study(storage=storage, study_name=name)
+    output = study.optimize(objective, n_trials=n_trials, n_jobs=n_jobs)
+    best_pars = study.best_params
+    print('best_pars = ', best_pars)
